b8e9d35a895ede36aa34814013e8d92a76f71454
Parker Strawbridge <parker.starwbridge@uvm.edu>
2020-10-14 14:59:07 -0400

    Add Basic UI window

---
+++ UI.py
@@ -0,0 +1,12 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((500, 400))
+pygame.display.set_caption('Othello')
+while True: # main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
